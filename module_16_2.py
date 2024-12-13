cc869c7ac2e3dd39e6f5d556129e6922d6c21b3b
aleksandr-verikon <sashava07@gmail.com>
2024-12-14 00:06:44 +0300

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -8,7 +8,7 @@ async def welcome() -> str:
     return "Главная страница"
 
 @app.get("/user/{username}/{age}")
-async def user_predator(username: Annotated[str, Path(min_lenght = 5, max_lenght = 20, description="Enter username", example = 'UrbanUser')]
+async def user_predator(username: Annotated[str, Path(min_length = 5, max_length = 20, description="Enter username", example = 'UrbanUser')]
                         , age: int = Path(ge=18, le=120, description="Enter age", example=24)) -> dict:
     return {"Информация о пользователе:" "Имя": username, "Возраст": age}
 
@@ -17,5 +17,5 @@ async def welcome_admin() -> str:
     return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
-async def welcome_user(user_id: int = Path(ge=1, le=100, description="Enter User ID", example=69)) -> dict:
+async def welcome_user(user_id: int = Path(ge=1, le=100, description="Enter User ID", examples=69)) -> dict:
     return {f"Вы вошли как пользователь № {user_id}"}
